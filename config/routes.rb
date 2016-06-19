7839ea07f3d07608ff6642afd6d33c67c693ca48
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-06-19 01:35:12 -0700

    Adds custome fields to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
+  resources :users, :only => [:show]
+
   devise_for :users
+
+#   authenticated :user do
+#   root to: "users#show", as: :authenticated_root
+# end
   root 'welcome#index'
 
 end
