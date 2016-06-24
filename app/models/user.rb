bce62258139a450b5b50a84ee1e86855e6cd88e2
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-06-24 00:49:11 -0700

    minor layout updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   NULL_ATTRS = %w( facebook twitter github linkedin)
   before_save :nil_if_blank
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('missing.png')
+  has_attached_file :avatar, styles: { medium: "300x300>", small: "20x20#" , thumb: "150x150#" }, :default_url => ActionController::Base.helpers.asset_path('missing.png')
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   # Include default devise modules. Others available are:
