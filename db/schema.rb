3eb7d5e68aeb145941db4129bb4fa2a668bd49b6
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-06-25 22:45:25 -0700

    Added User_info to Project Flow

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625052332) do
+ActiveRecord::Schema.define(version: 20160625080026) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160625052332) do
     t.string   "facebook"
     t.string   "linkedin"
     t.string   "github"
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
