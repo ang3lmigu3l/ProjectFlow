c170b26d94e159ff899300d0d6b2e36e826a731d
Miguel Angel Quintana <quintana.mige@gmail.com>
2016-06-28 14:03:01 -0700

    adds Validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,13 +5,16 @@ class Project < ActiveRecord::Base
   before_save { self.code_type ||= :other }
 
     has_attached_file :preview, styles: { large: "550x550>", medium: "300x300> ", small: "250x250#", thumb: "100x100#"},  :default_url => ActionController::Base.helpers.asset_path('preview.png')
+    
     validates_attachment_content_type :preview, content_type: /\Aimage\/.*\Z/
+    validates :title, length: {maximum: 15}, presence: true
+    validates :description, length: {minimum: 150}, presence: true
+    validates :body, length: {maximum:150}, presence: true
 
     default_scope {order('date_started DESC')}
-
     scope :completed, -> {where(completed: true)}
-
     scope :random_projects, -> {where(completed: false)}
+
 
     def nil_if_blank
       NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
